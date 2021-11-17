085f4a04b4e0a4a805a9e58732edb29dafd92c10
reza <personal.rezasajadi@gmail.com>
2021-11-17 16:14:22 +0330

    Refactoring video slice.

--- src/store/slice/VideoSlice.js
+++ src/store/slice/VideoSlice.js
@@ -4,24 +4,28 @@ import { GetVideos } from "../../containers/Video/Services/VideoService";
 export const getVideos = createAsyncThunk("videos", () => GetVideos());
 
 const videoSlice = createSlice({
-  name: "mainData",
+  name: "videos",
   initialState: {
-    mainData: {},
+    genres: [],
+    data: [],
     isLoading: false,
     error: null,
   },
   extraReducers: {
     [getVideos.pending]: (state) => {
-      state.mainData = {};
+      state.genres = [];
+      state.data = [];
       state.isLoading = true;
       state.error = null;
     },
     [getVideos.fulfilled]: (state, action) => {
-      state.mainData = action.payload;
+      state.genres = action.payload.genres;
+      state.data = action.payload.videos;
       state.isLoading = false;
     },
     [getVideos.rejected]: (state, action) => {
-      state.mainData = {};
+      state.genres = [];
+      state.data = [];
       state.isLoading = false;
       state.error = action.error?.message;
     },
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import mainDataReducer from "./slice/VideoSlice";
+import videoReducer from "./slice/VideoSlice";
 
 export default configureStore({
   reducer: {
-    mainData: mainDataReducer,
+    videos: videoReducer,
   },
 });
