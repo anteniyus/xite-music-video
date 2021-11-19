28ffdd6d7072b042e7b1018dace8e2c4dbb35cef
reza <personal.rezasajadi@gmail.com>
2021-11-19 19:40:37 +0330

    Adding retry button

--- src/containers/Video/List.js
+++ src/containers/Video/List.js
@@ -1,23 +1,76 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/styles";
+import { Button } from "@material-ui/core";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import { useSnackbar } from "notistack";
 import SectionSearchForm from "./Sections/SectionSearchForm";
 import SectionVideoItems from "./Sections/Video/SectionVideoItems";
 import SectionPagination from "./Sections/SectionPagination";
 import { getVideos } from "../../store/slice/VideoSlice";
 import Container from "../../components/Card/Container";
 
+const useStyles = makeStyles((theme) => ({
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "2rem",
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const VideoList = () => {
+  const classes = useStyles();
+
+  const didMountRef = useRef(false);
+
   const dispatch = useDispatch();
+
+  const { error } = useSelector((state) => state.videos);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const retryButton = (
+    <div className={classes.buttonContainer}>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<RefreshIcon />}
+        onClick={() => dispatch(getVideos())}
+      >
+        Retry
+      </Button>
+    </div>
+  );
 
   useEffect(() => {
     dispatch(getVideos());
   }, []);
 
+  useEffect(() => {
+    if (didMountRef.current) {
+      if (error)
+        enqueueSnackbar(error, {
+          variant: "error",
+        });
+    } else didMountRef.current = true;
+  }, error);
+
   return (
     <Container>
       <SectionSearchForm />
-      <SectionVideoItems />
-      <SectionPagination />
+
+      {error ? (
+        retryButton
+      ) : (
+        <>
+          <SectionVideoItems />
+          <SectionPagination />
+        </>
+      )}
     </Container>
   );
 };
