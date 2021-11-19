d56b2bd540f0bf1eec062ab482cb1f9d0ada6140
reza <personal.rezasajadi@gmail.com>
2021-11-19 16:56:42 +0330

    Adding test for search form section.

---
+++ src/containers/Video/Sections/tests/SectionSearchForm.test.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import AppTheme from "../../../../AppTheme";
+import SectionSearchForm from "../SectionSearchForm";
+
+const mockStore = configureStore([]);
+
+describe("Search Form", () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      videos: {
+        genres: [],
+
+        data: [],
+        filteredData: [],
+        currentData: [],
+
+        page: 1,
+
+        isLoading: false,
+        error: null,
+      },
+    });
+
+    store.dispatch = jest.fn();
+  });
+
+  test("Checks the DOMs", () => {
+    render(
+      <Provider store={store}>
+        <AppTheme>
+          <SectionSearchForm />
+        </AppTheme>
+      </Provider>
+    );
+
+    expect(screen.getByText("Search")).toBeInTheDocument();
+    expect(screen.getByText("Genres")).toBeInTheDocument();
+    expect(screen.getByText("Years")).toBeInTheDocument();
+  });
+
+  test("Checks the search value", () => {
+    const rendered = render(
+      <Provider store={store}>
+        <AppTheme>
+          <SectionSearchForm />
+        </AppTheme>
+      </Provider>
+    );
+
+    const input = rendered.getByLabelText("Search");
+
+    fireEvent.change(input, { target: { value: "test" } });
+
+    expect(input.value).toBe("test");
+  });
+
+  test("Checks the dispatch", async () => {
+    const query = "test";
+
+    const rendered = render(
+      <Provider store={store}>
+        <AppTheme>
+          <SectionSearchForm />
+        </AppTheme>
+      </Provider>
+    );
+
+    const input = rendered.getByLabelText("Search");
+
+    fireEvent.change(input, { target: { value: query } });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(query.length);
+  });
+});
